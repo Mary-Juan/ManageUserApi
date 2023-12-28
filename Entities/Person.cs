7865ca334b3335129b2f6d7204869ffd31f5133b
mobocomputer <mobocomputer@TEST-SYSTEM>
2023-12-28 09:01:13 -0800

    I implemented GenericRepository.

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -3,10 +3,11 @@ namespace ManageUserApi.Entities
 {
     public class Person
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public bool IsDeleted { get; set; } = false;
         public Role Role { get; set; }
     }
 }
